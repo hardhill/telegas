a1e0b945a7f7b35817079ce39ededca4c0e55fe8
W.Bielecki <hardhill@mail.ru>
2021-04-05 17:29:30 +0800

    menu

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,41 @@
 <template>
   <v-app>
-    <v-navigation-drawer app>
-      <!-- -->
+    <v-navigation-drawer permanent>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+              dense
+              nav
+      >
+        <v-list-item
+                v-for="item in items"
+                :key="item.title"
+                link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
     <v-app-bar app>
-      <!-- -->
+      <v-toolbar-title>TelegaS</v-toolbar-title>
     </v-app-bar>
 
     <!-- Sizes your content based upon application components -->
